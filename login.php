8d517f8e36a4f20d4b6f847d24e7fc561c99dc4a
mschumann15 <marcus.eshs@gmail.com>
2015-12-24 12:33:23 -0800

    addin in the php and ordering things

---
+++ login.php
@@ -0,0 +1,21 @@
+<?php include('includes/header.php'); ?>
+<body>
+    <div class="header-section action-shot2" title="Securing servers at the CyberPatriot VI finals in 2014.">
+        <div class="fade"></div>
+        <div class="title">
+            <div class="body2">
+                <div class="section">
+                    <form class="center" action="action_page.php">
+                        <h1 id="s" class="center">Sign in</h1>
+                        <p>  Email:</p>
+                        <input class="center" type="text" name="firstname">
+                        <br /><br />
+                        <p>  Password:</p>
+                        <input class="center" type="text" name="lastname">
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
